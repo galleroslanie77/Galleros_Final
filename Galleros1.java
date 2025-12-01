e1e18e48c65bd3b7c9e72a73bdb6428f6632df11
galleroslanie77 <galleroslanie77@gmail.com>
2025-12-01 10:33:39 +0800

    Add files via upload
    December 1, 2025

---
+++ Galleros1.java
@@ -0,0 +1,43 @@
+
+/**
+ * Write a description of class Galleros1 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+
+public class Galleros1
+{
+   public static void main(String [] args){
+       // Scanner sc = new Scanner(System.in);     1
+       // int num = 0;
+       
+       // while(num < 1 || num > 100){
+           // System.out.print("Enter a number between 1 to 100: ");
+           // num = sc.nextInt();
+       // }
+       // System.out.println("You entered: " + num);
+    //2
+          // Scanner sc = new Scanner(System.in);
+          // int[] numbers = new int[10];
+          // int count = 0;
+          
+          // for (int i = 0; i < 10; i++){
+              // System.out.print("Enter number " + (i + 1)+": ");
+              // numbers[i] = sc.nextInt();
+              
+              // for(int num : numbers){
+                  // if(num == 7){
+                    // count++;
+                  // }
+              // }
+          // }
+          // System.out.print("The number 7 appears " + count + "times. ");
+          int lines = 5;
+          
+          for (int i = 1; i <= lines; i++){
+              
+          }
+   }
+}
