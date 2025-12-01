66196d7b58ce55659a2495affa7e90ea7a1365a1
Galleros <galleroslanie77@gmail.com>
2025-12-01 10:47:32 +0800

    Add files via upload

---
+++ Galleros2.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Galleros2 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+public class Galleros2
+{
+    public static void main(String [] args){
+        int lines = 5;
+        
+        for (int i = 1; i <= lines; i++){
+            for (int j = 1; j <=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
