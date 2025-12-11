e397744f9a3088c1dcdda00711dd48528420e347
Galleros <galleroslanie77@gmail.com>
2025-12-11 08:03:20 +0800

    Clean up commented code in Galleros1.java
    Removed commented-out code related to user input and number counting.

--- Galleros1.java
+++ Galleros1.java
@@ -18,26 +18,5 @@ public class Galleros1
            // num = sc.nextInt();
        // }
        // System.out.println("You entered: " + num);
-    //2
-          // Scanner sc = new Scanner(System.in);
-          // int[] numbers = new int[10];
-          // int count = 0;
-          
-          // for (int i = 0; i < 10; i++){
-              // System.out.print("Enter number " + (i + 1)+": ");
-              // numbers[i] = sc.nextInt();
-              
-              // for(int num : numbers){
-                  // if(num == 7){
-                    // count++;
-                  // }
-              // }
-          // }
-          // System.out.print("The number 7 appears " + count + "times. ");
-          int lines = 5;
-          
-          for (int i = 1; i <= lines; i++){
-              
-          }
    }
 }
