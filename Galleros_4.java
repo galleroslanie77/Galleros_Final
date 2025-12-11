82bb15da6bd6dca7c3172f82a2e72a34a6119d4c
Galleros <galleroslanie77@gmail.com>
2025-12-11 09:09:57 +0800

    Update Galleros_4.java

--- Galleros_4.java
+++ Galleros_4.java
@@ -9,27 +9,15 @@ public class Galleros_4
 {
 public static void main(String[] args) {
 
-        int[] arr = {5, 2, 9, 1, 7};
-
-        System.out.print("Array: ");
-        for (int i=0;i<arr.length;i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println(); 
-
+        int[] arr = {10, 3, 25, -5, 0};
         int min = arr[0];
         int max = arr[0];
 
-        for (int j=0;j<arr.length;j++) {
-            if(arr[j] < min){
-                min = arr[j];
-            }
-            if(arr[j] > max){
-                max = arr[j];
-            }
+        for (int n : arr) {
+            if (n < min) min = n;
+            if (n > max) max = n;
         }
 
         System.out.println("Smallest: " + min + ", Largest: " + max);
-        
     }
 }
