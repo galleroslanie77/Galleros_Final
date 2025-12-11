343b437fefda3ff6f0b5724e2d29b33c318783ad
Galleros <galleroslanie77@gmail.com>
2025-12-11 08:41:46 +0800

    Update Galleros3.java

--- Galleros3.java
+++ Galleros3.java
@@ -8,8 +8,6 @@
 public class Galleros3
 {
     public static void main(String[] args) {
-        int lines = 5;
-
         for (int i = 1; i <= lines; i++) {
             for (int j = 1; j <= i; j++) { 
                 System.out.print("*");
