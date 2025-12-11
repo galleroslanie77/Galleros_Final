c50463cc025e1c090bd5c676e3ee38f773ff5a40
Galleros <galleroslanie77@gmail.com>
2025-12-11 08:28:36 +0800

    Refactor Galleros2 to use predefined array

--- Galleros2.java
+++ Galleros2.java
@@ -5,25 +5,13 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-import java.util.Scanner;
-public class Galleros2
-{
+public class Galleros2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] numbers = new int[10];
+        int[] arr = {1, 7, 3, 7, 5, 7, 2,7, 9, 10};
         int count = 0;
-
-        for (int i = 0; i < 10; i++) {
-            System.out.print("Enter number " + (i + 1) + ": ");
-            numbers[i] = sc.nextInt();
+        for (int n : arr) {
+            if (n == 7) count++;
         }
-
-        for (int num : numbers) {
-            if (num == 7) {
-                count++;
-            }
-        }
-
-        System.out.println("The number 7 appears " + count + " times.");
+        System.out.println("Number 7 appears " + count + " times.");
     }
 }
